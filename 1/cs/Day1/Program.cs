36d6ac459248ff17b39e73394e9f6a4c7fcedb3a
Michael Lesniak <mail@mlesniak.com>
2023-11-23 14:39:45 +0100

    Part 1

---
+++ 1/cs/Day1/Program.cs
@@ -0,0 +1,16 @@
+﻿string input =  File.ReadAllText("input.txt");
+int floor = 0;
+
+foreach (var c in input)
+{
+    if (c == '(')
+    {
+        floor++;
+    }
+    else
+    {
+        floor--;
+    }
+}
+
+Console.WriteLine($"{floor}");
