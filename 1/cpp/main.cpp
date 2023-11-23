c9c17c4071fe7360d96097f9419b0e949fbb18e4
Alexander-Nikolai Gomes <alexander-nikolai.gomes@elunic.com>
2023-11-23 20:25:44 +0100

    cpp: day1 part2

--- 1/cpp/main.cpp
+++ 1/cpp/main.cpp
@@ -5,17 +5,26 @@ using namespace std;
 
 int main() {
     int floor = 0;
+    int charPos = 1;
     char n;
     fstream fileIn("input.txt", fstream::in);
 
     while (fileIn >> n)
     {
+        
         if (n == '(') {
             floor++;
         } else if (n == ')')
         {
             floor--;
         }
+        if (floor == -1)
+        {
+            cout << charPos << endl;
+        }
+        
+        charPos++;
+        
     }
     cout << floor << endl;
 }
