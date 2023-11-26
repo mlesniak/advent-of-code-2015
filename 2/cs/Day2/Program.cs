78ff034bc6e64d5f1350ee03cf7c88bec3f6af11
Michael Lesniak <mail@mlesniak.com>
2023-11-26 18:26:15 +0100

    Day 2 / part 1

--- 2/cs/Day2/Program.cs
+++ 2/cs/Day2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] lines = File.ReadAllLines("input.txt");
+var sum = 0;
+
+foreach (string line in lines)
+{
+    int[] lwh = line.Split("x").Select(Int32.Parse).ToArray();
+    int l = lwh[0];
+    int w = lwh[1];
+    int h = lwh[2];
+    int paper = 2 * l * w + 2 * w * h + 2 * l * h;
+    int extra = Math.Min(l * w, Math.Min(w * h, l * h));
+    sum += (paper + extra);
+}
+
+Console.WriteLine(sum);
