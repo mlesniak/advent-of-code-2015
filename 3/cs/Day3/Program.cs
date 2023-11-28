261c43a5788fdd96f882e7ced72ef2cc1387b434
Michael Lesniak <mail@mlesniak.com>
2023-11-28 21:50:05 +0100

    Part 2

--- 3/cs/Day3/Program.cs
+++ 3/cs/Day3/Program.cs
@@ -16,11 +16,6 @@ public class Day3
         // 2, since robo is also there
         visitedHouses[santaStart] = 2;
 
-        //  x x
-        // x x
-        // 0123
-        // ^>v<
-
         for (int idx = 0; idx < directions.Length; idx++)
         {
             if (idx % 2 == 0)
@@ -37,76 +32,55 @@ public class Day3
         for (int santaChoice = 0; santaChoice < directions.Length; santaChoice += 2)
         {
             var dir = directions[santaChoice];
-            switch (dir)
-            {
-                case '^':
-                    santaStart = santaStart with {Y = santaStart.Y + 1};
-                    break;
-                case 'v':
-                    santaStart = santaStart with {Y = santaStart.Y - 1};
-                    break;
-                case '>':
-                    santaStart = santaStart with {X = santaStart.X + 1};
-                    break;
-                case '<':
-                    santaStart = santaStart with {X = santaStart.X - 1};
-                    break;
-
-            }
-            if (visitedHouses.TryGetValue(santaStart, out int count))
-            {
-                visitedHouses[santaStart] = count + 1;
-            }
-            else
-            {
-                visitedHouses[santaStart] = 1;
-            }
+            santaStart = ComputeNewPosition(dir, santaStart);
+            UpdateHouseCount(visitedHouses, santaStart);
         }
-        
+       
+        // Loop for robot.
         for (int roboChoice = 1; roboChoice < directions.Length; roboChoice += 2)
         {
             var dir = directions[roboChoice];
-            switch (dir)
-            {
-                case '^':
-                    roboStart = roboStart with {Y = roboStart.Y + 1};
-                    break;
-                case 'v':
-                    roboStart = roboStart with {Y = roboStart.Y - 1};
-                    break;
-                case '>':
-                    roboStart = roboStart with {X = roboStart.X + 1};
-                    break;
-                case '<':
-                    roboStart = roboStart with {X = roboStart.X - 1};
-                    break;
-
-            }
-            if (visitedHouses.TryGetValue(roboStart, out int count))
-            {
-                visitedHouses[roboStart] = count + 1;
-            }
-            else
-            {
-                visitedHouses[roboStart] = 1;
-            }
+            roboStart = ComputeNewPosition(dir, roboStart);
+            UpdateHouseCount(visitedHouses, roboStart);
         }
 
         // Result.
         Console.WriteLine(visitedHouses.Count);
     }
+
+    private static void UpdateHouseCount(Dictionary<Position, int> visitedHouses, Position position)
+    {
+        if (visitedHouses.TryGetValue(position, out int count))
+        {
+            visitedHouses[position] = count + 1;
+        }
+        else
+        {
+            visitedHouses[position] = 1;
+        }
+    }
+
+    private static Position ComputeNewPosition(char dir, Position position)
+    {
+        switch (dir)
+        {
+            case '^':
+                position = position with {Y = position.Y + 1};
+                break;
+            case 'v':
+                position = position with {Y = position.Y - 1};
+                break;
+            case '>':
+                position = position with {X = position.X + 1};
+                break;
+            case '<':
+                position = position with {X = position.X - 1};
+                break;
+        }
+        return position;
+    }
 }
 
 
-// Santa and Robo-Santa start at the same location (delivering two presents
-// to the same starting house), then take turns moving based on instructions
-
-// ^>v<
-// (0/0) santa
-// (0/0) robo-santa
-// ^ -> 0/1  santa
-// > -> 1/0  robo
-// v -> 0/0  santa
-// < -> 0/0  robo
 
 
