974940f7e9e4780f92f32eef0d0beb794220d19a
Michael Lesniak <mail@mlesniak.com>
2023-11-28 21:27:00 +0100

    WIP

--- 3/cs/Day3/Program.cs
+++ 3/cs/Day3/Program.cs
@@ -9,42 +9,104 @@ public class Day3
         // Console.WriteLine(directions);
 
         var visitedHouses = new Dictionary<Position, int>();
-        var start = new Position(0, 0);
-        visitedHouses[start] = 1;
-        foreach (var dir in directions)
-        {
-            // Console.WriteLine(dir);
-            switch (dir)
-            {
-                case '^':
-                    start = start with {Y = start.Y + 1};
-                    break;
-                case 'v':
-                    start = start with {Y = start.Y - 1};
-                    break;
-                case '>':
-                    start = start with {X = start.X + 1};
-                    break;
-                case '<':
-                    start = start with {X = start.X - 1};
-                    break;
 
-            }
-            if (visitedHouses.TryGetValue(start, out int count))
+
+        var santaStart = new Position(0, 0);
+        var roboStart = new Position(0, 0);
+        // 2, since robo is also there
+        visitedHouses[santaStart] = 2;
+
+        //  x x
+        // x x
+        // 0123
+        // ^>v<
+
+        for (int idx = 0; idx < directions.Length; idx++)
+        {
+            if (idx % 2 == 0)
             {
-                visitedHouses[start] = count + 1;
+                // Santa
             }
             else
             {
-                visitedHouses[start] = 1;
+                // Robo.
             }
         }
 
-        // foreach (var house in visitedHouses)
-        // {
-        //     Console.WriteLine(house);
-        // }
+        // Loop for santa
+        for (int santaChoice = 0; santaChoice < directions.Length; santaChoice += 2)
+        {
+            var dir = directions[santaChoice];
+            switch (dir)
+            {
+                case '^':
+                    santaStart = santaStart with {Y = santaStart.Y + 1};
+                    break;
+                case 'v':
+                    santaStart = santaStart with {Y = santaStart.Y - 1};
+                    break;
+                case '>':
+                    santaStart = santaStart with {X = santaStart.X + 1};
+                    break;
+                case '<':
+                    santaStart = santaStart with {X = santaStart.X - 1};
+                    break;
 
+            }
+            if (visitedHouses.TryGetValue(santaStart, out int count))
+            {
+                visitedHouses[santaStart] = count + 1;
+            }
+            else
+            {
+                visitedHouses[santaStart] = 1;
+            }
+        }
+        
+        for (int roboChoice = 1; roboChoice < directions.Length; roboChoice += 2)
+        {
+            var dir = directions[roboChoice];
+            switch (dir)
+            {
+                case '^':
+                    roboStart = roboStart with {Y = roboStart.Y + 1};
+                    break;
+                case 'v':
+                    roboStart = roboStart with {Y = roboStart.Y - 1};
+                    break;
+                case '>':
+                    roboStart = roboStart with {X = roboStart.X + 1};
+                    break;
+                case '<':
+                    roboStart = roboStart with {X = roboStart.X - 1};
+                    break;
+
+            }
+            if (visitedHouses.TryGetValue(roboStart, out int count))
+            {
+                visitedHouses[roboStart] = count + 1;
+            }
+            else
+            {
+                visitedHouses[roboStart] = 1;
+            }
+        }
+
+        // Result.
         Console.WriteLine(visitedHouses.Count);
     }
 }
+
+
+// Santa and Robo-Santa start at the same location (delivering two presents
+// to the same starting house), then take turns moving based on instructions
+
+// ^>v<
+// (0/0) santa
+// (0/0) robo-santa
+// ^ -> 0/1  santa
+// > -> 1/0  robo
+// v -> 0/0  santa
+// < -> 0/0  robo
+
+
