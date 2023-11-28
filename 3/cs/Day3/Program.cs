fadb0cee89df86825d8823bc33e0fedb39e74928
Michael Lesniak <mail@mlesniak.com>
2023-11-28 21:15:51 +0100

    Day 3 part 1

--- 3/cs/Day3/Program.cs
+++ 3/cs/Day3/Program.cs
@@ -1,3 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿record Position(int X, int Y);
 
-Console.WriteLine("Hello, World!");
+
+public class Day3
+{
+    public static void Main(string[] args)
+    {
+        string directions = File.ReadAllText("input.txt");
+        // Console.WriteLine(directions);
+
+        var visitedHouses = new Dictionary<Position, int>();
+        var start = new Position(0, 0);
+        visitedHouses[start] = 1;
+        foreach (var dir in directions)
+        {
+            // Console.WriteLine(dir);
+            switch (dir)
+            {
+                case '^':
+                    start = start with {Y = start.Y + 1};
+                    break;
+                case 'v':
+                    start = start with {Y = start.Y - 1};
+                    break;
+                case '>':
+                    start = start with {X = start.X + 1};
+                    break;
+                case '<':
+                    start = start with {X = start.X - 1};
+                    break;
+
+            }
+            if (visitedHouses.TryGetValue(start, out int count))
+            {
+                visitedHouses[start] = count + 1;
+            }
+            else
+            {
+                visitedHouses[start] = 1;
+            }
+        }
+
+        // foreach (var house in visitedHouses)
+        // {
+        //     Console.WriteLine(house);
+        // }
+
+        Console.WriteLine(visitedHouses.Count);
+    }
+}
